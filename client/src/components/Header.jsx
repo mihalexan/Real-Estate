9318baec024d839404c07d7ff6178bef36dc2f39
Mihalexan <alexandra1mihalexan@gmail.com>
2024-07-05 21:20:19 +0200

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,35 +3,35 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-black shadow-md">
+    <header className="bg-white shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-50">Real</span>
+            <span className="text-slate-950">Real</span>
             <span className="text-red-500">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-50 p-3 rounded-lg flex items-center">
+        <form className="bg-white border p-3 rounded-lg flex items-center shadow-md">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64 md:w-45"
+            className="bg-transparent focus:outline-none w-24 sm:w-64 md:w-45 "
           />
           <FaSearch className="text-slate-950" />
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-50 hover:underline">
+            <li className="hidden sm:inline text-slate-950 hover:underline">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-50 hover:underline">
+            <li className="hidden sm:inline text-slate-950 hover:underline">
               About
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className=" text-slate-50 hover:underline">Sign in</li>
+            <li className=" text-slate-950 hover:underline">Sign in</li>
           </Link>
         </ul>
       </div>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>Sign In</div>;
+  return (
+    <div>
+      Sign In
+      <Link to={"/sign-up"}>
+        <span className="text-blue-700">Sign up</span>
+      </Link>
+    </div>
+  );
 }
